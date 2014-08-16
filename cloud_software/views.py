ff2c8d48c0c29d93060cd970c3191f7d3c21a683
Jan Paricka <jparicka@gmail.com>
2014-08-16 16:58:58 +0200

    started cloud software template

---
+++ cloud_software/views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import pickle
+import logging
+import datetime
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+logger = logging.getLogger(__name__)
+
+from django.contrib.auth.models import User
+from userprofile.models import Profile as userprofile
+
+
+def cloud_software(request):
+
+	print '-- cloud_software:'
+
+	if not request.user.is_authenticated():
+		return HttpResponseRedirect("/")
+
+	user = request.user
+	profile = userprofile.objects.get(user=request.user)
+	secret = profile.secret
+
+	print request.user
+
+	return render_to_response('cloud_software.html', {'user':user,'profile':profile,}, context_instance=RequestContext(request))
