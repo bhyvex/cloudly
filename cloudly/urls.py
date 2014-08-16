faf79082d65954f4b7f1da990f3df85806c1a3d7
Jan Paricka <jparicka@gmail.com>
2014-08-16 12:28:01 +0200

    added on basic url schema

--- cloudly/urls.py
+++ cloudly/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cloudly.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 	url(r'^$', 'dashboard.views.home', name='home'),
 )
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+import os
+import time
+import pickle
+import logging
+import datetime
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+logger = logging.getLogger(__name__)
+
+def home(request):
+		
+	if not request.user.is_authenticated():
+		
+		print '--  web:'
+		print 'anonymous'
+	
+		return render_to_response('web.html', locals(), context_instance=RequestContext(request))
+
+	print '--  dashboard:'
+	print 'anonymous'
+	
+	return render_to_response('dashboard.html', {'active_tab':active_tab,}, context_instance=RequestContext(request))
