0012ea2ea47162aea841ef336b12294e95d94ffa
Jan Paricka <jparicka@gmail.com>
2014-08-30 19:47:30 +0200

    initiated simple cloud storage view

--- private_storage/views.py
+++ private_storage/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+import os
+import time
+import pickle
+import logging
+import datetime
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+from django.contrib.auth.models import User
+from userprofile.models import Profile as userprofile
+
+logger = logging.getLogger(__name__)
+
+import pymongo
+from pymongo import MongoClient
+from pymongo import ASCENDING, DESCENDING
+client = MongoClient('localhost', 27017)
+
+mongo = client.cloudly
+
+def private_storage(request):
+	return HttpResponse("working on this currently")
