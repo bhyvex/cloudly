b73692d77a58dd7d1806bafb049ef1b6c4bb4546
Jan Paricka <jparicka@gmail.com>
2014-08-31 21:20:14 +0200

    added on better lock wallpapers

--- static/admin-assets/js/pages/page-lockscreen.js
+++ static/admin-assets/js/pages/page-lockscreen.js
@@ -8,6 +8,10 @@ $.backstretch([
   	"/static/images/lockscreen/7.jpg",
   	"/static/images/lockscreen/8.jpg",
   	"/static/images/lockscreen/9.jpg",
+  	"/static/images/lockscreen/10.jpg",
+  	"/static/images/lockscreen/11.jpg",
+  	"/static/images/lockscreen/12.jpg",
+  	"/static/images/lockscreen/13.jpg",
 ], {
     fade: 1000,
     duration: 5000
