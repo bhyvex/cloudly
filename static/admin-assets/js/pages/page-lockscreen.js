7b1de256f58281614484ab527fe90193bdddc307
Jan Paricka <jparicka@gmail.com>
2014-08-31 18:37:02 +0200

    added on more lock screen artwork

--- static/admin-assets/js/pages/page-lockscreen.js
+++ static/admin-assets/js/pages/page-lockscreen.js
@@ -5,6 +5,9 @@ $.backstretch([
   	"/static/images/lockscreen/4.jpg",
   	"/static/images/lockscreen/5.jpg",
   	"/static/images/lockscreen/6.jpg",
+  	"/static/images/lockscreen/7.jpg",
+  	"/static/images/lockscreen/8.jpg",
+  	"/static/images/lockscreen/9.jpg",
 ], {
     fade: 1000,
     duration: 5000
