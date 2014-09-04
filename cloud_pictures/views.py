75a9e8126ad1cf6a8dd658a462467c16ec925511
Jan Paricka <jparicka@gmail.com>
2014-09-04 23:16:24 +0200

    added on required models onto newly added app pictures

--- cloud_pictures/views.py
+++ cloud_pictures/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+import os
+import time
+import pickle
+import logging
+import datetime
+
+from pprint import pprint
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+logger = logging.getLogger(__name__)
+
+import boto.ec2
+import boto.ec2.cloudwatch
+
+from django.contrib.auth.models import User
+from userprofile.models import Profile as userprofile
+
+from amazon import s3_funcs
+from amazon import s3_funcs_shortcuts
+
+from django import forms
+from forms import UploadFileForm
+from cloud_storage.models import Files
+from cloud_storage.models import Uploaded_Files
+
+
+
