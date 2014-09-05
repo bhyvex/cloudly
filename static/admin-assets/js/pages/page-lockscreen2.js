95408784edfc37131fe52610ec7f27230bcf03ed
Jan Paricka <jparicka@gmail.com>
2014-09-05 22:33:27 +0200

    js lockscreen cleansing

--- static/admin-assets/js/pages/page-lockscreen2.js
+++ static/admin-assets/js/pages/page-lockscreen2.js
@@ -1,18 +1,18 @@
 $.backstretch([
-  	"/static/images/lockscreen/1.jpg",
-  	"/static/images/lockscreen/2.jpg",
-  	"/static/images/lockscreen/3.jpg",
-  	"/static/images/lockscreen/4.jpg",
-  	"/static/images/lockscreen/5.jpg",
-  	"/static/images/lockscreen/6.jpg",
-  	"/static/images/lockscreen/7.jpg",
-  	"/static/images/lockscreen/8.jpg",
-  	"/static/images/lockscreen/9.jpg",
-  	"/static/images/lockscreen/10.jpg",
-  	"/static/images/lockscreen/11.jpg",
-  	"/static/images/lockscreen/12.jpg",
-  	"/static/images/lockscreen/13.jpg"
+ 	"/static/images/lockscreen/1.jpg",
+ 	"/static/images/lockscreen/2.jpg",
+ 	"/static/images/lockscreen/3.jpg",
+ 	"/static/images/lockscreen/4.jpg",
+ 	"/static/images/lockscreen/5.jpg",
+ 	"/static/images/lockscreen/6.jpg",
+ 	"/static/images/lockscreen/7.jpg",
+ 	"/static/images/lockscreen/8.jpg",
+ 	"/static/images/lockscreen/9.jpg",
+ 	"/static/images/lockscreen/10.jpg",
+ 	"/static/images/lockscreen/11.jpg",
+ 	"/static/images/lockscreen/12.jpg",
+  	"/static/images/lockscreen/13.jpg",
 ], {
     fade: 1000,
-    duration: 16000
+    duration: 4000
 });
