d3ab8276564e90e310abe78e79c4a4c7e79855fc
Kamil Hurajt <info@softdream.net>
2014-09-22 00:23:56 +0200

    Add awesome feature for multi jquery upload file. Look at this!

--- static/admin-assets/js/dropzone-fileupload.js
+++ static/admin-assets/js/dropzone-fileupload.js
@@ -3,16 +3,17 @@ $('#upload').fileupload({
   uploadTemplateId: null,
   downloadTemplateId: null,
   dataType: 'json',
-  
+  limitConcurrentUploads: 3,
   uploadTemplate: function(o){
       
       var rows = $();
       $.each(o.files, function(index, file){
-        var tpl = $('<tr class="template-upload fade">'+
+        var fileId = hex_md5(file.name+file.size);
+        var tpl = $('<tr id="'+fileId+'" class="template-upload fade">'+
                           '<td align="center"><span class="preview"></span></td>'+
                           '<td class="filename"></td>'+
                           '<td class="filesize"></td>'+
-                          '<td class="status"><div class="progress"></div></td>'+
+                          '<td class="status"><div class="progress"><span class="progressCount">0%</span><div class="progress-bar progress-bar-success" style="width:0%;"></div></div></td>'+
                           '<td class="action">'+
                               (!index && !o.options.autoUpload ?
                               '<button class="btn btn-sm btn-primary start" disabled>Upload</button>' : '') +
@@ -36,7 +37,7 @@ $('#upload').fileupload({
       var rows = $();
       $.each(o.files, function(index, file){
         var tpl = $('<tr class="template-download fade">'+
-                          '<td align="center"><span class="preview"></span></td>'+
+                          '<td align="center"><span class="prev'+index+' preview"></span></td>'+
                           '<td class="filename"></td>'+
                           '<td class="filesize"></td>'+
                           '<td class="status"><span class="label label-warning">Synchronising..</span></td>'+
@@ -48,19 +49,19 @@ $('#upload').fileupload({
                      '</tr>');
             
             
-            
+            console.log(file.thumbnailUrl);
             tpl.find('td.filename').text(file.name);
             tpl.find('td.filesize').append('<i>'+formatFileSize(parseInt(file.size))+'</i>');
             
             if(file.thumbnailUrl){
-                var img = $('img');
-                img.prop('src',file.thumbnailUrl);
-                img.prop('width','80');
-                tpl.find(".preview").append(img);
+                var imgTag = $("<img>");
+                imgTag.prop('src',file.thumbnailUrl);
+                imgTag.prop('width','80');
+                tpl.find(".preview").html(imgTag);
             }
             rows = rows.add(tpl);
       });
-      
+      console.log(rows);
       return rows;
   },
   progressall: function (e, data) {
@@ -69,9 +70,29 @@ $('#upload').fileupload({
             'width',
             progress + '%'
         );
-    }
+
+        $("#progressAll span.progressCount").text(progress+"%");
+    },
+  progress: function(e, data){
+      var file = data.files[0];
+      var fileId = hex_md5(file.name+file.size);
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      var element = $("#"+fileId);
+      element.find('.progress-bar').css('width', progress+'%');
+      element.find(".progress span.progressCount").text(progress+'%');
+      
+      if(progress >= 100){
+          element.find('.progress').fadeOut();
+          var loader = $("<img>");
+          loader.prop('src','/static/images/loading.gif');
+          loader.prop('width','25');
+          
+          element.find('td.status').css("text-align","center").html(loader);
+      }
+  }  
   
 });
+
 //Helper function for calculation of progress
 function formatFileSize(bytes) {
     if (typeof bytes !== 'number') {
