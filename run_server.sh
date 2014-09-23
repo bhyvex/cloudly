5bfd9298ea9f8874ac6a0d978f401c61823c975e
Jan Paricka <jparicka@gmail.com>
2014-09-23 16:28:51 +0200

    fixing path on server run script

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp -rf settings.py-server settings.py
-python2.7 manage.py runfcgi daemonize=false method=threaded host=127.0.0.1 port=8080 pidfile=/var/run/fuckmecloudly.pid --settings=settings.py
+cp -rf cloudly/settings.py-server cloudly/settings.py
+python2.7 manage.py runfcgi daemonize=false method=threaded host=127.0.0.1 port=8080 pidfile=/var/run/fuckmecloudly.pid --settings=cloudly.settings
+
 
