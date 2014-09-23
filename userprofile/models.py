5c664cede889cb9fd86bd1275f13930868f4083f
Jan Paricka <jparicka@gmail.com>
2014-09-23 18:14:22 +0200

    changing activity user model to foreignkey

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Activity(models.Model):
 	
-	user = models.OneToOneField(User)
+	user = models.ForeignKey(User)
 	link = models.URLField(blank=True, verbose_name="links")
 	activity = models.CharField(max_length=100, blank=True, verbose_name="activities", db_index=True)
 	date_created = models.DateTimeField(auto_now_add=True)
