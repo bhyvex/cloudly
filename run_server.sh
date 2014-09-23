6d97546c3087c7e94c2ff6fecbede5620d557d4e
Jan Paricka <jparicka@gmail.com>
2014-09-23 16:27:22 +0200

    one more attempt to get the server run script working

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp -rf settings.py-server settings.py
-python2.7 manage.py runfcgi daemonize=false method=threaded host=127.0.0.1 port=8080 pidfile=/var/run/fuckmecloudly.pid --settings=fuckmecloudly.settings
+python2.7 manage.py runfcgi daemonize=false method=threaded host=127.0.0.1 port=8080 pidfile=/var/run/fuckmecloudly.pid --settings=settings.py
+
