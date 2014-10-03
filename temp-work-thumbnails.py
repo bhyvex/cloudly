82dd9f6ec2d35e9b8f2b48c879277ce183956c04
Jan Paricka <jparicka@gmail.com>
2014-10-03 22:29:25 +0200

    cleaned up devel comments on the resize code

--- temp-work-thumbnails.py
+++ temp-work-thumbnails.py
@@ -13,21 +13,14 @@ from PIL import Image
 
 
 def resize(img, box, fit, out):
-	'''Downsample the image.
-	@param img: Image -  an Image-object
-	@param box: tuple(x, y) - the bounding box of the result image
-	@param fix: boolean - crop the image to fill the box
-	@param out: file-like-object - save the image into the output stream
-	'''
-	# Credits: http://united-coders.com/christian-harms/image-resizing-tips-every-coder-should-know/
-	#preresize image with factor 2, 4, 8 and fast algorithm
+	# Pre-resize image with factor 2, 4, 8 and fast algorithm
 	factor = 1
 	while img.size[0]/factor > 2*box[0] and img.size[1]*2/factor > 2*box[1]:
 		factor *=2
 	if factor > 1:
 		img.thumbnail((img.size[0]/factor, img.size[1]/factor), Image.NEAREST)
 
-	#calculate the cropping box and get the cropped part
+	# Calculate the cropping box and get the cropped part
 	if fit:
 		x1 = y1 = 0
 		x2, y2 = img.size
@@ -41,9 +34,9 @@ def resize(img, box, fit, out):
 			x2 = int(x2/2+box[0]*hRatio/2)
 		img = img.crop((x1,y1,x2,y2))
 
-	#Resize the image with best quality algorithm ANTI-ALIAS
+	# Resize the image with best quality algorithm ANTI-ALIAS
 	img.thumbnail(box, Image.ANTIALIAS)
-	#save it into a file-like object
+	# Save it into a file-like object
 	img.save(out, "JPEG", quality=75)
 
 
