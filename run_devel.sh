cdcc401d255f5755b783f25bdd91d0ace8a795e7
Jan Paricka <jparicka@gmail.com>
2014-10-04 22:55:48 +0200

    cosmetic output updates to the run devel script

--- run_devel.sh
+++ run_devel.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+
 #echo syncing db
 #python manage.py syncdb
+
+#echo
 echo updating from git....
 git pull
+
+echo
 echo running development server..
 python manage.py runserver 0.0.0.0:8000
