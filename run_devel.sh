7dd341932b685b5ea9c5f3487401002ca6ff0de5
Jan Paricka <jparicka@gmail.com>
2014-10-04 22:51:15 +0200

    added on explanations onto the devel run script

--- run_devel.sh
+++ run_devel.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-echo running development server
+#echo syncing db
 #python manage.py syncdb
+echo updating from git....
 git pull
+echo running development server..
 python manage.py runserver 0.0.0.0:8000
