a597df7a871285dc1f1b9df7e60dc09f9661861b
Jan Paricka <jparicka@gmail.com>
2014-10-04 23:16:37 +0200

    fixing terminal issues with the run script output

--- run_devel.sh
+++ run_devel.sh
@@ -2,14 +2,14 @@
 
 clear
 
-echo ** Synchronizing the database..
+echo !! Synchronizing the database..
 python manage.py syncdb
 
 echo
-echo ** Updating to the latest version from git....
+echo !! Updating to the latest version from git....
 git pull
 
 echo
-echo ** Running development server..
+echo !! Running development server..
 python manage.py runserver 0.0.0.0:8000
 
