6fa8b487e3f559c5c8c3ccff5f7a49fbc9faa613
Jan Paricka <jparicka@gmail.com>
2014-10-06 20:46:11 +0200

    decommissioning VMs update on the dasboard

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -38,10 +38,95 @@ from cloudly.templatetags.cloud_extras import clear_filename, get_file_extension
 
 
 def ajax_vms_refresh(request):
-	# XXX work on this
-	import time
-	import random
-	time.sleep(random.randint(1,5))
+	
+	user = request.user
+	profile = userprofile.objects.get(user=request.user)
+	
+	aws_access_key = profile.aws_access_key
+	aws_secret_key = profile.aws_secret_key
+	aws_ec2_verified = profile.aws_ec2_verified
+	
+	aws_virtual_machines = {}
+
+	if aws_ec2_verified:
+					
+		aws_regions = profile.aws_enabled_regions.split(',')
+		print 'AWS regions', aws_regions
+		
+		for ec2_region in aws_regions:
+			
+			if(ec2_region):
+
+				ec2conn = boto.ec2.connect_to_region(ec2_region,aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key)
+				cloudwatch = boto.ec2.cloudwatch.connect_to_region(ec2_region,aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key)
+
+				reservations = ec2conn.get_all_instances()
+				instances = [i for r in reservations for i in r.instances]
+
+				for instance in instances:
+					
+					instance_metrics = {}
+					instance_metrics['instance'] = instance.__dict__
+					
+					#pprint(instance.__dict__)
+										
+					print '** name', instance.id
+					print '** monitoring', instance.monitoring_state
+					
+					if(instance.monitoring_state=="disabled"):
+						try:
+							ec2conn.monitor_instance(str(instance.id))
+						except:
+							print instance.id, 'instance not in a monitorable state!'.upper()
+							print instance.id, 'state:', instance.state
+							print instance.id, 'reason:', instance.state_reason['message']
+							continue
+					
+					end = datetime.datetime.utcnow()
+					start = end - datetime.timedelta(hours=1)
+				
+					# ['Minimum', 'Maximum', 'Sum', 'Average', 'SampleCount']
+					# ['Seconds', 'Percent', 'Bytes', 'Bits', 'Count', 'Bytes/Second', 'Bits/Second', 'Count/Second']
+					
+					# CPUUtilization
+					metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="CPUUtilization")[0]
+					cpu_utilization_datapoints = metric.query(start, end, 'Average', 'Percent')
+					instance_metrics['cpu_utilization_datapoints'] = cpu_utilization_datapoints
+
+					# DiskReadOps
+					#metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="DiskReadOps")[0]
+					#disk_readops_datapoints = metric.query(start, end, 'Average', '')
+					#instance_metrics['disk_readops_datapoints'] = disk_readops_datapoints
+
+					# DiskWriteOps
+					#metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="DiskWriteOps")[0]
+					#disk_writeops_datapoints = metric.query(start, end, 'Average', '')
+					#instance_metrics['disk_writeops_datapoints'] = disk_writeops_datapoints
+
+					# DiskReadBytes
+					metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="DiskReadBytes")[0]
+					disk_readbytes_datapoints = metric.query(start, end, 'Average', '')
+					instance_metrics['disk_readbytes_datapoints'] = disk_readbytes_datapoints
+
+					# DiskWriteBytes
+					metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="DiskWriteBytes")[0]
+					disk_writebytes_datapoints = metric.query(start, end, 'Average', '')
+					instance_metrics['disk_writebytes_datapoints'] = disk_writebytes_datapoints
+					
+					# NetworkIn
+					metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="NetworkIn")[0]
+					networkin_datapoints = metric.query(start, end, 'Average', '')
+					instance_metrics['networkin_datapoints'] = networkin_datapoints
+					
+					# NetworkOut
+					metric = cloudwatch.list_metrics(dimensions={'InstanceId':instance.id}, metric_name="NetworkOut")[0]
+					networkout_datapoints = metric.query(start, end, 'Average', '')
+					instance_metrics['networkout_datapoints'] = networkout_datapoints
+
+					aws_virtual_machines[instance.id] = instance_metrics
+
+	print 'vms update comple'
+		
 	return HttpResponse("ALLDONE")
 
 
