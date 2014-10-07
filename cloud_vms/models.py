51d8ca4465ac784f3936a2334794081292dd7a69
Jan Paricka <jparicka@gmail.com>
2014-10-07 19:04:46 +0200

    tidied up devel notes on the cloud vms models

--- cloud_vms/models.py
+++ cloud_vms/models.py
@@ -6,16 +6,8 @@ class Cache(models.Model):
 	
 	user = models.ForeignKey(User)
 	
-	# XXX Note: perhaps it's a good idea to work this on mongo?
-	
-	# vms_info1
-	# vms_info2
-	# vms_info3
-	# vms_info4
-	# vms_info5
-	# vms_info6
-	# vms_info7	
-	# vms_info8
+	# vms_respose
+	# vms_info_filtered_1
 
 	last_seen = models.DateTimeField(auto_now_add=True)
 	date_created = models.DateTimeField(auto_now_add=True)
