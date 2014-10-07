1b667f2d775e8a6f61ad1f3714c73e5919ddfad4
Jan Paricka <jparicka@gmail.com>
2014-10-07 12:33:30 +0200

    added on devel notes onto the cloud VMs models

--- cloud_vms/models.py
+++ cloud_vms/models.py
@@ -8,12 +8,15 @@ class Cache(models.Model):
 	
 	# XXX Note: perhaps it's a good idea to work this on mongo?
 	
-	# vms_detail_level1
-	# vms_detail_level2
-	# vms_detail_level3
-	# vms_detail_level4
-	# vms_detail_level5
-	
+	# vms_info1
+	# vms_info2
+	# vms_info3
+	# vms_info4
+	# vms_info5
+	# vms_info6
+	# vms_info7	
+	# vms_info8
+
 	last_seen = models.DateTimeField(auto_now_add=True)
 	date_created = models.DateTimeField(auto_now_add=True)
 	
