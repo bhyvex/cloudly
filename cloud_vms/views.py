fa5a446f37a8d5f22708fa392c3ed0a38a1c95e2
Jan Paricka <jparicka@gmail.com>
2014-10-10 18:42:13 +0200

    fixing the invalid syntax problem in vms update

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -167,7 +167,7 @@ def ajax_vms_refresh(request):
 	try:
 		vms_cache.vms_response = base64.b64encode(pickle.dumps(aws_virtual_machines, pickle.HIGHEST_PROTOCOL))	
 	except:
-		vms_cache.vms_response = json.dumps(aws_virtual_machines),default=date_handler)
+		vms_cache.vms_response = json.dumps(aws_virtual_machines,default=date_handler)
 		
 	
 	
