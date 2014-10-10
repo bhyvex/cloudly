c4a8665e4214426ab941923c4cf89e0328041c1d
Jan Paricka <jparicka@gmail.com>
2014-10-10 17:59:24 +0200

    trying out with json loads function

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -162,7 +162,8 @@ def ajax_vms_refresh(request):
 	except:
 		from collections import defaultdict
 		from functools import partial
-		vms_cache.vms_response = pickle.dumps(base64.b64encode(aws_virtual_machines))
+		import json
+		vms_cache.vms_response = pickle.dumps(base64.b64encode(json.loads(aws_virtual_machines)))
 	
 	
 	from django.utils import timezone
