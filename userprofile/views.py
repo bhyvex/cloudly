d5457fd7b9d55505329828e0f550c33697237e09
Jan Paricka <jparicka@gmail.com>
2014-10-10 13:08:14 +0200

    added on form of validating AWS on the cloud settings

--- userprofile/views.py
+++ userprofile/views.py
@@ -268,18 +268,23 @@ def cloud_settings_update_credentials(request):
 		aws_access_key_id=aws_access_key,
 		aws_secret_access_key=aws_secret_key)
 
-		cloudwatch = boto.ec2.cloudwatch.connect_to_region( profile_regions[1],
-		aws_access_key_id=aws_access_key,
-		aws_secret_access_key=aws_secret_key)
+		# XXX this is validation
+		
+		regions_ = ec2conn.get_all_regions()
 
-		reservations = ec2conn.get_all_instances()
-		instances = [i for r in reservations for i in r.instances]
+
+		#cloudwatch = boto.ec2.cloudwatch.connect_to_region( profile_regions[1],
+		#aws_access_key_id=aws_access_key,
+		#aws_secret_access_key=aws_secret_key)
+
+		#reservations = ec2conn.get_all_instances()
+		#instances = [i for r in reservations for i in r.instances]
 
 		print '-'*1000
 		print 'ec2conn', ec2conn
-		print 'cloudwatch', cloudwatch
-		print 'reservations', reservations
-		print 'instances', instances
+		#print 'cloudwatch', cloudwatch
+		#print 'reservations', reservations
+		#print 'instances', instances
 		
 	
 	return render_to_response('cloud_settings.html', {'err':err,'aws_regions':AWS_REGIONS,'profile_regions':profile_regions,'profile':profile,'secret':secret,}, context_instance=RequestContext(request))
