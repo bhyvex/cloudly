f0f392f81b52e8322211bedfb9e28f5d5a8debab
Jan Paricka <jparicka@gmail.com>
2014-10-12 17:07:26 +0200

    reforming groups on the instance response

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -111,16 +111,14 @@ def ajax_vms_refresh(request):
 					for volume in ec2conn.get_all_volumes(filters={'attachment.instance-id': instance.id}):
 						volumes[volume] = [volume.id, volume.iops, volume.size, volume.type,]
 
-					
-					groups = {}
+					groups = []
 					for group in instance_metrics['instance']['groups']:
-						groups[group] = [group.id, group.name,]
+						groups.append([group.id, group.name,])
 
 
 					# XXX these are in fact objects hence cannot be flattened..
-					#aws_virtual_machines[instance.id]['instance']['groups'] = []
-					#aws_virtual_machines[instance.id]['instance']['block_device_mapping'] = []
-
+					instance_metrics['instance']['groups'] = groups
+					instance_metrics['instance']['block_device_mapping'] = volumes
 					
 					try:
 						ec2conn.monitor_instance(str(instance.id))
