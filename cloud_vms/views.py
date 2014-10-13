81eaf3812a3b46352495f7df3aeb91514a279f9b
Jan Paricka <jparicka@gmail.com>
2014-10-13 16:50:50 +0200

    added on crucial devel notes on the vm view

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -238,6 +238,9 @@ def aws_vm_view(request,vm_name):
 	vms_cache = Cache.objects.get(user=user)
 	vm_cache =  vms_cache.vms_response
 	
+	# XXX check permissions to see this VM!!!!!!!!!!!!
+	
+	
 	return render_to_response('aws_vm.html', {'vm_name':vm_name,'vm_cache':vm_cache,}, context_instance=RequestContext(request))
 
 
