510dccaab88beb0a268697a8d10f6a4200683659
Jan Paricka <jparicka@gmail.com>
2014-10-13 13:47:17 +0200

    cleaned up debugging on the ajax vms

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -139,11 +139,9 @@ def ajax_vms_refresh(request):
 					instance_metrics['tags'] = instance.tags
 					instance_metrics['virtualization_type'] = instance.virtualization_type
 					instance_metrics['vpc_id'] = instance.vpc_id
-					
-					pprint(instance.__dict__)
-					
 					instance_metrics['region'] = {"endpoint":instance.region.endpoint,"name":instance.region.name,}				
 					instance_metrics['state'] = {"state":instance.state,"code":instance.state_code,"state_reason":instance.state_reason,}
+
 				
 					try:
 						ec2conn.monitor_instance(str(instance.id))
