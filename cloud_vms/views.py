50c24dd01dcb8ead85b153586b227798a3774578
Jan Paricka <jparicka@gmail.com>
2014-10-13 14:05:27 +0200

    added on debugging on the aws cpu metrics

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -165,7 +165,9 @@ def ajax_vms_refresh(request):
 					
 					cpu_utilization_datapoints_ = []
 					cpu_utilization_datapoints = metric.query(start, end, 'Average', 'Percent')
-					for i in cpu_utilization_datapoints: cpu_utilization_datapoints_.append(i)
+					for i in cpu_utilization_datapoints: 
+						cpu_utilization_datapoints_.append(i)
+						print i
 					instance_metrics['cpu_utilization_datapoints'] = cpu_utilization_datapoints_
 
 					# DiskReadOps
