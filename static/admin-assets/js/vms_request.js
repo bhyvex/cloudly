5baa9c3b1c6df3c5558a5750bb9573c9ffd8ff01
Kamil Hurajt <info@softdream.cz>
2014-10-19 14:18:15 +0200

    Fix dual bug request call

--- static/admin-assets/js/vms_request.js
+++ static/admin-assets/js/vms_request.js
@@ -4,10 +4,6 @@ var vmsRequest = {
 	firstRun: true,
 	initAction: function(){
 		var $this = this;
-//		if(this.firstRun === true){
-//			this.firstRun = false;
-//			this.start();
-//		}
 		setInterval(function(){
 			
 			if($this.hasResponse === true || $this.firstRun === true){
@@ -23,7 +19,7 @@ var vmsRequest = {
 		$.ajax({
 			type: 'GET',
 			url: '/ajax/cloud/vms/refresh/',
-			data: '',
+			data: 'nodata=nodata',
 			success:function(res){
 				if(res.indexOf('ALLDONE') !== -1){
 					$this.hasResponse = true;
