99ed6dc35d826724d183670e81956f681c05b052
Jan Paricka <jparicka@gmail.com>
2014-10-21 14:21:17 +0200

    Even more debugging..

--- cloud_storage/views.py
+++ cloud_storage/views.py
@@ -225,7 +225,7 @@ def dropzone_uploader(request):
 			new_file = Files(file=request.FILES['files[]'])
 			new_file.save()
 		except: 
-			return HttpResponse("empty files - " + request.FILES['files[]'])
+			return HttpResponse("request - " + request)
 			
 		f = Uploaded_Files.objects.create(file=new_file,user=request.user)
 
