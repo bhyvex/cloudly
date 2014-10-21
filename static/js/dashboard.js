5daa0ab00b1d7ac0d2e4ea52da71f48048897179
Kamil Hurajt <info@softdream.net>
2014-10-21 11:08:48 +0200

    test slowest reload and animations

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -24,7 +24,7 @@ var cloudlyVMSmanager  = {
                 $this.template = res;
                 setInterval(function(){
                     $this.loadMachinesData();
-                },1000);
+                },5000);
             }
         });
     },
