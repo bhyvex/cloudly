a3bbe374dedae5ddab29a1e1b1dc0a8ef5078d35
Jan Paricka <jparicka@gmail.com>
2014-10-26 17:22:46 +0100

    Updated ec2 aws functions, mostly cosmetics and exceptions.

--- amazon/ec2_funcs.py
+++ amazon/ec2_funcs.py
@@ -14,42 +14,30 @@ import boto.ec2.cloudwatch
 
 def aws_instance_start(name, region):
 
-	try:
-		aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
-		instance = aws_conn.get_all_instances(instance_ids=[name,])
-		instance[0].instances[0].start()
-	except: pass
-	
+	aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
+	instance = aws_conn.get_all_instances(instance_ids=[name,])
+	instance[0].instances[0].start()
 	return False
 
 
 def aws_instance_stop(name, region):
 	
-	try:
-		aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
-		instance = aws_conn.get_all_instances(instance_ids=[name,])
-		instance[0].instances[0].stop()
-	except: pass
-	
+	aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
+	instance = aws_conn.get_all_instances(instance_ids=[name,])
+	instance[0].instances[0].stop()
 	return False
 
 
 def aws_instance_reboot(name, region):
 
-	try:
-		aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
-		instance = aws_conn.get_all_instances(instance_ids=[name,])
-		instance[0].instances[0].reboot()
-	except: pass
-	
+	aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
+	instance = aws_conn.get_all_instances(instance_ids=[name,])
+	instance[0].instances[0].reboot()	
 	return False
 
 def aws_instance_terminate(name, region):
 
-	try:
-		aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
-		instance = aws_conn.get_all_instances(instance_ids=[name,])
-		instance[0].instances[0].terminate()
-	except: pass
-	
+	aws_conn = boto.ec2.connect_to_region(region,aws_access_key_id=AWS_ACCESS_KEY,aws_secret_access_key=AWS_ACCESS_SECRET)
+	instance = aws_conn.get_all_instances(instance_ids=[name,])
+	instance[0].instances[0].terminate()
 	return False
