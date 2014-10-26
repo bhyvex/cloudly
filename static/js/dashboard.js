09bcc7820f7103bf6545e66e373b6450f6f84a12
Kamil Hurajt <info@softdream.cz>
2014-10-26 18:06:48 +0100

    Add generated link from bakcend to template

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -87,7 +87,7 @@ var cloudlyVMSmanager  = {
     },
     addVMS: function(vms,data){
         var template = this.template;
-        template = template.replace('{@vm@}',vms);
+        template = template.replace('{@vmlink@}',data.link);
         template = template.replace('{@vm@}',vms);
         template = template.replace('{@vm@}',vms);
         template = template.replace('{@vmtitle@}',data.vmtitle);
