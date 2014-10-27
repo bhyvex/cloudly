2c2cf60849c1090c1266a4ccb30e62ae34408f3c
Jan Paricka <jparicka@gmail.com>
2014-10-27 13:22:43 +0100

    Introducing datetime special for the JS graphs.

--- cloudly/templatetags/cloud_extras.py
+++ cloudly/templatetags/cloud_extras.py
@@ -74,6 +74,12 @@ def get_tags(package):
 	return Tags.objects.filter(package=package)
 
 
+
+@register.filter(name='format_datetime_special')
+def format_datetime_special(date):
+	return date
+
+
 @register.filter(name='count_user_files')
 def count_user_files(user):
 	
