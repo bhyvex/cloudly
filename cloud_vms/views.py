b45aa918b284ce70db0c233a0afac34e3a628b59
Jan Paricka <jparicka@gmail.com>
2014-10-27 13:49:03 +0100

    Adjusting how many data are being pulled on each request.

--- cloud_vms/views.py
+++ cloud_vms/views.py
@@ -169,7 +169,7 @@ def ajax_vms_refresh(request):
 
 					# Here is where you define start - end for the Logs...............
 					end = datetime.datetime.utcnow()
-					start = end - datetime.timedelta(hours=1)
+					start = end - datetime.timedelta(hours=6)
 				
 					# This is how you list all possible values on the response....
 					# print ec2conn.list_metrics()
