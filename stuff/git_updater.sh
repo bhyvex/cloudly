44b1118fc488d3504928e2c1cea2b752c7bf3116
Jan Paricka <jparicka@gmail.com>
2015-01-06 18:20:18 +0000

    Added on handy git updater script....

---
+++ stuff/git_updater.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 15 seconds prior to launching next git update...; sleep 15; done
+  
+
