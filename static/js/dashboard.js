ed814a5b97acf61133e1e4f22fb42c27bebd6c81
Jan Paricka <jparicka@gmail.com>
2015-01-14 10:06:53 +0100

    cleaned up debugging from the dashboard.js

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -80,7 +80,6 @@ var cloudlyVMSmanager  = {
                         console.log('VM '+vms+' changed color: '+color+' to: '+data.vmcolor+' and actual has class: '+panel.attr('class'));
                         return;
                 }
-                console.log('here color:', data.vmcolor)
         }); 
 		
     },
