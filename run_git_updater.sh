ae0b41d5034825ed5d9964e082f60cf5db9fb9f9
Jan Paricka <jparicka@gmail.com>
2015-01-14 21:50:13 +0100

    shorten git updater

--- run_git_updater.sh
+++ run_git_updater.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 15 seconds prior to launching the next git update...; ls -la; sleep 15; done
+while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 1 second prior to launching the next git update...; ls -la; sleep 1; done
   
 
