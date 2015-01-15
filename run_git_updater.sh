02f9304dd51e7a397508af9977d192a01a6a4704
Jan Paricka <jparicka@gmail.com>
2015-01-15 17:51:21 +0100

    further shorten delay on the git updater

--- run_git_updater.sh
+++ run_git_updater.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 5 seconds prior to launching the next git update...; sleep 5; done
+while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 3 seconds prior to launching the next git update...; sleep 3; done
   
 
