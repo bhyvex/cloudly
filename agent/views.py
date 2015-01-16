951873541be1e331dc824172fb1fb2764b96e077
Jan Paricka <jparicka@gmail.com>
2015-01-16 05:25:11 -0500

    Added on base modules on the download agent view.

--- agent/views.py
+++ agent/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import logging
+import datetime
+import base64, pickle
+
+from pprint import pprint
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+logger = logging.getLogger(__name__)
+
+from django.contrib.auth.models import User
+from userprofile.models import Profile as userprofile
+from userprofile.views import _log_user_activity
+
+
+def download_agent(request):
+	return HttpResponse("working on this currently")
