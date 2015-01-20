b52448107a793ce0692afd7edaa4dca017795ebc
Jan Paricka <jparicka@gmail.com>
2015-01-20 12:39:50 +0100

    revert isotope replace function

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -86,8 +86,8 @@ var cloudlyVMSmanager  = {
     addVMS: function(vms,data){
         var template = this.template;
         template = template.replace('{@vmlink@}',data.link);
-        template = template.replace('{@vm@}',vms.replace(/:/g,'-'));
-        template = template.replace('{@vm@}',vms.replace(/:/g,'-'));
+        template = template.replace('{@vm@}',vms);
+        template = template.replace('{@vm@}',vms);
         template = template.replace('{@vmtitle@}',data.vmtitle);
         template = template.replace('{@vmcolor@}',data.vmcolor);
         template = template.replace('{@averge@}',data.averge);
