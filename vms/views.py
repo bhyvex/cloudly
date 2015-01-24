b733738fe4cd31347c8963c6bae928405e5c8411
Jan Paricka <jparicka@gmail.com>
2015-01-24 08:23:29 +0100

    settling with 60 minutes report on the boto vms

--- vms/views.py
+++ vms/views.py
@@ -207,7 +207,7 @@ def ajax_vms_refresh(request):
 
 					# Here is where you define start - end for the Logs...............
 					end = datetime.datetime.utcnow()
-					start = end - datetime.timedelta(minutes=10)
+					start = end - datetime.timedelta(minutes=60)
 				
 					# This is how you list all possible values on the response....
 					# print ec2conn.list_metrics()
