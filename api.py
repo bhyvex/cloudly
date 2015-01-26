8d5a6027e80b95648ade9923358d97ab2ef582a6
Jan Paricka <jparicka@gmail.com>
2015-01-26 18:20:09 +0100

    cosmetic update to the api debugging

--- api.py
+++ api.py
@@ -84,8 +84,8 @@ def ping():
 	}
 
 	print 'API query from v'+str(agent_version), uuid
-	print '    '+unichr(746), secret, 'IP', ip_remote+'/'+ ip
-	print '    '+unichr(746)+' host machine uptime '+uptime+''
+	print ' '+unichr(746), secret, 'IP', ip_remote+'/'+ ip
+	print ' '+unichr(746)+' host machine uptime '+uptime+''
 
 	#print server
 	#import random
