a9c7e9e6a887111a63c238f781dd51e1eb03fa7d
Jan Paricka <jparicka@gmail.com>
2015-01-26 10:05:37 +0100

    added on provider into the isotope vm name

--- vms/views.py
+++ vms/views.py
@@ -383,6 +383,7 @@ def ajax_virtual_machines(request):
 			try:
 				if(vm_cache[vm]["instance"]["tags"]["Name"]):
 					instance_name = vm_cache[vm]["instance"]["tags"]["Name"]
+					instance_name = "(aws)" + instance_name
 				else:
 					instance_name = vm
 			except: instance_name = vm
