09adb30c7fe7d063a8a197b243f2bdfce074985c
Jan Paricka <jparicka@gmail.com>
2015-01-29 14:23:24 +0100

    Added on core modules onto the newly added devel.

--- devel/views.py
+++ devel/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import pickle
+import logging
+import datetime
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from django.http import HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+
+logger = logging.getLogger(__name__)
+
+from django.contrib.auth.models import User
+from userprofile.models import Profile
+from userprofile.views import _log_user_activity
+
+
+def devel(request):
+  return HttpResponse("devel")
