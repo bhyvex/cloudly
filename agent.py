444be9b853ea46120c69b3207fe4decd02d0f790
Jan Paricka <jparicka@gmail.com>
2015-02-13 16:45:19 +0100

    reordered code on the agent to make it more appealing

--- agent.py
+++ agent.py
@@ -17,15 +17,15 @@ import subprocess
 
 AGENT_VERSION = "0.1"
 
+if(not getpass.getuser()=="root"):
+    print 'Please re-run this script as user root.'
+    sys.exit(0)
+
 SECRET = "" # to be injected on download by Cloudly
 if(not SECRET): SECRET = raw_input("Enter your secret: ")
 
 API_SERVER = "" # to be injected on download by Cloudly
 if(not API_SERVER): API_SERVER = "127.0.0.1:5000"
-
-if(not getpass.getuser()=="root"):
-    print 'Please re-run this script as user root.'
-    sys.exit(0)
 
 HWADDR = subprocess.Popen(["ifconfig","eth0"], stdout=subprocess.PIPE, close_fds=True).communicate()[0]
 UUID = re.search(r'([0-9A-F]{2}[:-]){5}([0-9A-F]{2})', HWADDR, re.I).group()
