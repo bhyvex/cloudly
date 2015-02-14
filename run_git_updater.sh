5d7c05bc881591e0505f0c0e993409d5c61abede
Jan Paricka <jparicka@gmail.com>
2015-02-14 18:59:30 +0100

    deleting git updater as we no longer use it

--- run_git_updater.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-while true; do clear; echo executing$ git pull; echo Updating devel..; echo Pulling uupdates from git...; git pull; echo Sleeping 4 seconds prior to launching the next git update...; sleep 4; done
-  
-
