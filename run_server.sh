85f0c03ea44ba409d4f2bdc2dc17c1cd078e494d
Jan Paricka <jparicka@gmail.com>
2015-02-17 09:29:02 +0000

    Cleaned up server run script.

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-cp -rf cloudly/settings.py-server cloudly/settings.py
 python2.7 manage.py runfcgi daemonize=false method=threaded host=127.0.0.1 port=8080 pidfile=/var/run/projectcloudly.pid --settings=cloudly.settings
-
 
