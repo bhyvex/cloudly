b273a19284706c594898f8bd0e2ea4570f667506
Jan Paricka <jparicka@gmail.com>
2015-03-03 13:18:51 +0100

    replacing special characters from the isotope names

--- vms/views.py
+++ vms/views.py
@@ -91,7 +91,8 @@ def ajax_vms_refresh(request):
 			instance_metrics['instance']['user_id'] = request.user.id
 			instance_metrics['instance']['state'] = {}
 			instance_metrics['instance']['tags'] = {}
-			instance_metrics["instance"]['tags']['Name'] = server['hostname']
+			
+			instance_metrics["instance"]['tags']['Name'] = ''.join(x for x in unicodedata.normalize('NFKD', server['hostname']) if x in string.ascii_letters).lower()
 
 			uuid = server['uuid']		
 			cpu_usage = mongo.cpu_usage.find({'uuid':uuid,}).sort('_id',-1).limit(60)
