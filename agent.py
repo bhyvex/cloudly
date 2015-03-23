ca75d0041f203bcf2a8c7561f49d4be58b6fdd69
Jan Paricka <jparicka@gmail.com>
2015-03-23 14:26:06 +0100

    redoing networking data structure

--- agent.py
+++ agent.py
@@ -326,7 +326,7 @@ def _get_networking_stats():
     if(output_accept_packets>0): outbound_traffic['output_accept_packets'] = output_accept_packets
     if(output_accept_bytes>0): outbound_traffic['output_accept_bytes'] = output_accept_bytes
 
-    networking = {'inbound_traffic':inbound_traffic,'outbound_traffic':outbound_traffic,}
+    networking = {'input_accept_packets':input_accept_packets,'input_accept_bytes':input_accept_bytes,'output_accept_packets':output_accept_packets,'output_accept_byte':output_accept_byte,}
     proc = subprocess.Popen(['iptables','-Z'], stdout=subprocess.PIPE, close_fds=True)
 
     return networking
