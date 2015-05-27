2bbe583bfd635853cca68ee922c97b1190a8b5f4
Ondrej Kopal <koon@email.cz>
2015-05-28 00:25:38 +0200

    Update js by HighCharts documentation

--- static/js/graphs_ajax.js
+++ static/js/graphs_ajax.js
@@ -1,76 +1,69 @@
+var server = $('input[name="hwaddr"]').val();
+var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+var secret = $('input[name="secret"]').val();
+
+function requestCpuUsageData() {
+    var address = '/ajax/server/' + server + '/metrics/cpu_usage/';
+
+    $.ajax({
+        url: address,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'X-CSRFToken': csrf
+        },
+        cache: false,
+        data: {
+            'server': server,
+            'secret': secret
+        },
+        success: function(point) {
+            var series = cpuUsagechart.series[0];
+            var shift = series.data.length > 60;
+
+            // add the point
+            cpuUsagechart.series[0].addPoint(point, true, shift);
+
+            // call it again after one second
+            setTimeout(requestCpuUsageData, 1000);
+        }
+    });
+}
+
 $(document).ready(function() {
-	$('#cpu_usage').highcharts({
-		chart: {
+    var cpuUsagechart = new Highcharts.Chart({
+        chart: {
+            renderTo: '#cpu_usage',
             type: 'spline',
             events: {
-                load: function() {
-                    var series = this.series[0];
-                    console.log('series');
-                    console.log(series);
-
-                    setInterval(function () {
-                        load_cpu_usage_graph_ajax().done(function(data) {
-                            console.log('load data');
-                            console.log(data);
-
-                            series.setData(data);
-                        });
-                    }, 2000);
-                }
+                load: requestCpuUsageData
             }
         },
-		title: {
-			text: 'CPU Usage'
-		},
-		subtitle: {
-			text: ''
-		},
-		xAxis: {
-			type: 'datetime',
-			title: {
-				text: 'Datetime'
-			}
-		},
-		yAxis: {
-			title: {
-				text: 'CPU %'
-			},
-		},
-		min: 0,
-		tooltip: {
-			headerFormat: '<b>{series.name}:</b> {point.y:.2f}<br>',
-			      pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}'
-			 },
+        title: {
+            text: 'CPU Usage'
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            type: 'datetime',
+            title: {
+                text: 'Datetime'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'CPU %'
+            },
+        },
+        min: 0,
+        tooltip: {
+            headerFormat: '<b>{series.name}:</b> {point.y:.2f}<br>',
+            pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}'
+        },
         series: [{
             name: 'CPU Used',
-            data:
-		load_cpu_usage_graph_ajax().done(function(data) {
-			console.log('initialize data');
-			console.log(data);
-			return data;
-		})
+            data: []
         }]
     });
 });
-
-function load_cpu_usage_graph_ajax() {
-	var server = $('input[name="hwaddr"]').val();
-	var csrf = $('input[name="csrfmiddlewaretoken"]').val();
-	var secret = $('input[name="secret"]').val();
-
-	var address = '/ajax/server/' + server + '/metrics/cpu_usage/';
-
-	return $.ajax({
-		url: address,
-		type: 'POST',
-		dataType: 'json',
-		headers: {
-			'X-CSRFToken': csrf
-		},
-		cache: false,
-		data: {
-			'server': server,
-			'secret': secret
-		}
-	});
-}
