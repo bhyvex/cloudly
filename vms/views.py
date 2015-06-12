ce607502a969fbdf578ef2154826de3dc31df960
Jan Paricka <jparicka@gmail.com>
2015-06-12 17:12:51 +0200

    an attempt to get rd off unicode on the graphs response

--- vms/views.py
+++ vms/views.py
@@ -662,7 +662,7 @@ def ajax_server_graphs(request, hwaddr, graph_type=""):
 			c+=1
 
 		processes = processes_
-		processes = str(processes).replace("u'","'")
+		processes = str(processes).replace("u'","'").replace("/u","")
 		
 		print processes
 		
