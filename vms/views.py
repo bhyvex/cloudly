a77ead36542de64f7f474aeafabbba99c6480d5d
Jan Paricka <jparicka@gmail.com>
2015-06-15 16:19:22 +0200

    added on debugging onto the graphs js

--- vms/views.py
+++ vms/views.py
@@ -740,7 +740,9 @@ def ajax_server_graphs(request, hwaddr, graph_type=""):
 			graphs_mixed_respose.append([i,tsdb_response[i]])
 		
 		graphs_mixed_respose = str(graphs_mixed_respose).replace("u'","'")
-
+		print 'graphs_mixed_respose'*100
+		print graphs_mixed_respose
+		
 		return HttpResponse(graphs_mixed_respose, content_type="application/json")
 
 
