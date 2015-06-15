62fb0e1350f29253f91d10413487cfe147e91b62
Jan Paricka <jparicka@gmail.com>
2015-06-15 16:29:51 +0200

    fixing the invalid syntax problem on the ajax response

--- vms/views.py
+++ vms/views.py
@@ -740,7 +740,7 @@ def ajax_server_graphs(request, hwaddr, graph_type=""):
 		graphs_mixed_respose = []
 		
 		for i in tsdb_response:
-			graphs_mixed_respose.append([int(i),round(float(tsdb_response[i]],2))
+			graphs_mixed_respose.append([int(i),round(float(tsdb_response[i]),2)])
 		
 		graphs_mixed_respose = str(graphs_mixed_respose).replace("u'","'")
 		print 'graphs_mixed_respose'*100
