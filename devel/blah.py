70f70f598cf0ce9c3de5657e4099e99694b10119
Jan Paricka <jparicka@gmail.com>
2015-07-07 09:57:20 +0200

    started on devel get network session function

---
+++ devel/blah.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import re
+import time
+import getpass
+import datetime
+import platform
+import base64, pickle
+
+import urllib
+import httplib
+import subprocess
+
+
+def _get_network_sessions():
+    print 'Active Internet Connections (including servers)
+    
+    # XXX resolve services
+    
+    return ""
+    
+_get_network_sessions()
+
+    
