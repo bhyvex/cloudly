7017aefddb38f506329263ff12dd792ba62ebed2
Jan Paricka <jparicka@gmail.com>
2015-07-10 22:08:38 +0200

    updated sessions naming convention on the agent and one more

--- agent.py
+++ agent.py
@@ -541,7 +541,7 @@ def get_system_metrics( uuid, secret ):
         'disks_usage': disks_usage,
         'processes': processes,
         'networking': networking,
-        'network_sessions': network_connections,
+        'network_connections': network_connections,
         'agent_version': AGENT_VERSION,
     }
 
