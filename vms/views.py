df2969af6e6dac8ba13a95c4e5a5a4cb888b5077
Ondrej Kopal <koon@email.cz>
2015-07-12 13:25:02 +0000

    Synchronized server statuses

--- vms/views.py
+++ vms/views.py
@@ -324,7 +324,7 @@ def ajax_vms_refresh(request):
 
             if((datetime.datetime.utcnow()-server['last_seen']).total_seconds()>20):
                 instance_metrics['instance']['state']['state'] = "Stopped"
-                if((datetime.datetime.utcnow()-server['last_seen']).total_seconds()>600):
+                if((datetime.datetime.utcnow()-server['last_seen']).total_seconds()>1800):
                     cpu_usage = []
             else:
                 instance_metrics['instance']['state']['state'] = "Running"
