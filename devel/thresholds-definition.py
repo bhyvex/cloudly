eb8a9094978e8827e758b71e9f1ccd09272d56a0
Jan Paricka <jparicka@gmail.com>
2015-07-14 14:06:18 +0200

    devel code cleansing

--- devel/thresholds-definition.py
+++ devel/thresholds-definition.py
@@ -49,41 +49,7 @@ def _get_sys_loadavg():
     return loadavg
     
 
-
-def _get_sys_uptime():
     
-    uptime = subprocess.Popen(['uptime',], stdout=subprocess.PIPE, close_fds=True).communicate()[0]
-    uptime = re.findall("[ 0-9:]up[ ](.*)[,][ 0-9]+user", uptime)[0]
-
-    return uptime
-    
-    
-
-def _get_sys_cpu():
-    
-    cpu_info = subprocess.Popen(["ps","axo","pcpu"], stdout=subprocess.PIPE, close_fds=True).communicate()[0]
-
-    c=0
-    cpu_total = float(0)
-    
-    for line in cpu_info.split('\n'):
-
-        line = line.replace(' ','')
-        
-        try:
-            cpu_process_usage = float(line)
-            cpu_total += cpu_process_usage
-        except:
-            pass
-    
-    cpu_usage = {
-        'cpu_used':round(cpu_total,2),
-        'cpu_free':round(float(100-cpu_total),2)
-    }
-    
-    return cpu_usage
-    
-
 
 print _get_sys_loadavg()
 
