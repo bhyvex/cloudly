9f8664c9e6271dcc9be952612dc145be214cd554
Jan Paricka <jparicka@gmail.com>
2015-07-26 20:16:19 +0200

    reduced amount of debugging on the api

--- api.py
+++ api.py
@@ -173,12 +173,8 @@ def ping():
     hbase.send(loadavg_tsdb_cmd)
     hbase.close()
 
+    print 'debug loadavg_service_report', loadavg_service_report
     print '-' * 100
-    print 'loadavg_tsdb_cmd'
-    print loadavg_tsdb_cmd
-    print 'loadavg_service_report', loadavg_service_report
-    print '-' * 100
-
 
     memory_usage_metrics = {
         'secret': secret,
