fd79329b8f537ee91769526b5911fd21060404ce
Jan Paricka <jparicka@gmail.com>
2015-07-29 20:54:18 +0200

    added on agent allowed self update declaration

--- agent.py
+++ agent.py
@@ -20,6 +20,7 @@ try:
 except: pass
 
 AGENT_VERSION = "0.2"
+AGENT_ALLOWED_SELF_UPDATE = True
 
 SECRET = "" # to be injected on download by Cloudly
 if(not SECRET): SECRET = raw_input("Enter your secret: ")
