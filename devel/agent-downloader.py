8e44b3297737280341797d558a95481fc9954ec5
Jan Paricka <jparicka@gmail.com>
2015-07-30 11:30:41 +0200

    applying uuid and secret on the updater agent func

--- devel/agent-downloader.py
+++ devel/agent-downloader.py
@@ -21,17 +21,21 @@ AGENT_VERSION = "0.1"
 AGENT_ALLOWED_TO_SELF_UPDATE = True
 
 
-def self_update():
+def self_update( uuid, secret ):
 
     conn = httplib.HTTPSConnection("raw.githubusercontent.com")
     conn.request( "GET", "/jparicka/cloudly/master/agent.py" )
     r1 = conn.getresponse()
     data = r1.read()
 
-    print data
+    print type(data), len(data)
 
     return AGENT_VERSION
     
 
-print self_update()
+uuid = 'xxx'
+secret = 'secret'
+
+print self_update( uuid, secret )
+
 
