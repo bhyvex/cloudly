11c78b5dac90582f1bde516e65998bcd2761672d
Jan Paricka <jparicka@gmail.com>
2015-08-15 17:15:57 +0200

    added on more devel notes

--- vms/views.py
+++ vms/views.py
@@ -381,6 +381,7 @@ def server_view(request, hwaddr):
 
     # XXX services discovery to search partials within both command and the process!
     # XXX recognise and tag when server is virtual
+    # XXX update the info on the mongo server
 
 
     return render_to_response('server_detail.html', {'secret':profile.secret,'recently_clicked_servers':recently_clicked_servers, 'hwaddr':hwaddr,'hwaddr_orig':hwaddr_orig,'server':server,'server_status':server_status,'disks_usage':disks_usage,'disks':disks,'reduced_disks':reduced_disks,'mem_usage':mem_usage,'loadavg':loadavg,'networking':networking,'activity':activity,'recently_clicked_servers':recently_clicked_servers,}, context_instance=RequestContext(request))
