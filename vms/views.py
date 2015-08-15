a5a90f7fa9931cd173f92ac45f606a8a37a481f1
Jan Paricka <jparicka@gmail.com>
2015-08-15 15:20:40 +0200

    worked out service definition

--- vms/views.py
+++ vms/views.py
@@ -289,6 +289,7 @@ def server_view(request, hwaddr):
 
 
     services_discovered = []
+    service = {'process_name':"",'tag':""}
 
 
     return render_to_response('server_detail.html', {'secret':profile.secret,'recently_clicked_servers':recently_clicked_servers, 'hwaddr':hwaddr,'hwaddr_orig':hwaddr_orig,'server':server,'server_status':server_status,'disks_usage':disks_usage,'disks':disks,'reduced_disks':reduced_disks,'mem_usage':mem_usage,'loadavg':loadavg,'networking':networking,'activity':activity,'recently_clicked_servers':recently_clicked_servers,}, context_instance=RequestContext(request))
