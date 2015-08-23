5586c1ceb28826f0bf82ff2e74f831489cf462b1
Jan Paricka <jparicka@gmail.com>
2015-08-24 00:38:13 +0200

    unified investors view with the credits

--- dashboard/views.py
+++ dashboard/views.py
@@ -97,14 +97,17 @@ def credits(request):
 
 
 
-@login_required()
 def investors(request):
 
     print '--  investors page:', request.user
     
     ip = request.META['REMOTE_ADDR']
-    profile = userprofile.objects.get(user=request.user)
-    _log_user_activity(profile,"click","/investors/","welcome",ip=ip)
+    try:
+        profile = userprofile.objects.get(user=request.user)
+        _log_user_activity(profile,"click","/investors/","welcome",ip=ip)
+    except:
+        return HttpResponseRedirect("https://github.com/jparicka/cloudly")
+
     
     print request.user
     return render_to_response('investors.html', locals(), context_instance=RequestContext(request))
