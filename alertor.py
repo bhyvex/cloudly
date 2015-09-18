dd87fd9073b58c1c6a781e7837174e771077f069
Jan Paricka <jparicka@gmail.com>
2015-09-18 16:58:36 +0200

    added on standard layout on the alertor

---
+++ alertor.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8
+#!/usr/bin/env python
+
+import os
+import time
+import tweepy
+import json
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ippe.settings")
+django.setup()
+
+from profiles.models import Profile
+from django.contrib.auth.models import User
+
+if __name__ == "__main__":
+    print "hello world from alertor"
