6d4cde02a5e2cc9779008f032df2382c14e48dd9
Jan Paricka <jparicka@gmail.com>
2015-10-18 17:37:01 +0000

    general debug cleansing

--- api.py
+++ api.py
@@ -349,13 +349,10 @@ def ping():
         disks_usage_service_report,
     ]
 
-    print 10
-
     active_service_statuses = mongo.active_service_statuses
     historical_service_statuses = mongo.historical_service_statuses
     alertor_queue = mongo.alertor_queue
 
-    print 20
 
     for status_report in service_statuses__to_process:
         
