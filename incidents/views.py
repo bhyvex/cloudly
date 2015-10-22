56ca6bb00b464cfb5298e651015d3f18af53154b
Jan Paricka <jparicka@gmail.com>
2015-10-22 14:08:30 +0000

    calculating notifications duration

--- incidents/views.py
+++ incidents/views.py
@@ -58,4 +58,4 @@ def incidents(request):
     critical_notifs = active_service_statuses.find({"secret":secret,"current_overall_status":"CRITICAL"})
 
 
-    return render_to_response('incidents.html', {'request':request, 'notifs_counter':notifs_counter,'active_service_statuses':active_service_statuses_data,'unknown_notifs':unknown_notifs,'warning_notifs':warning_notifs,'critical_notifs':critical_notifs,'profile':profile,}, context_instance=RequestContext(request))
+    return render_to_response('incidents.html', {'request':request,'notifs_counter':notifs_counter,'active_service_statuses':active_service_statuses_data,'unknown_notifs':unknown_notifs,'warning_notifs':warning_notifs,'critical_notifs':critical_notifs,'profile':profile,}, context_instance=RequestContext(request))
