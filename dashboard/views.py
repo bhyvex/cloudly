375cdc46e331826f5db3ed8839a62c30c7d9e9ab
Jan Paricka <jparicka@gmail.com>
2015-10-22 15:04:00 +0000

    and one more added in more mottos

--- dashboard/views.py
+++ dashboard/views.py
@@ -48,6 +48,7 @@ def home(request):
             "Real-time servers monitoring.",
             "Real-time and very playful monitoring.",
             "Does the server monitoring for you like no other.",
+            "Cheerful server monitoring.",
         ]
         return render_to_response('web.html', {'request':request,'CLOUDLY_MOTTOS':CLOUDLY_MOTTOS,}, context_instance=RequestContext(request))
 
