5ff2c587b079abf110f9bee18a637f789919c405
Jan Paricka <jparicka@gmail.com>
2015-11-16 12:41:38 +0100

    added on mongo bits and bobs on the alertor

--- alertor.py
+++ alertor.py
@@ -10,8 +10,22 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloudly.settings")
 django.setup()
 
-from profiles.models import Profile
+from userprofile.models import Profile
 from django.contrib.auth.models import User
+
+
+import pymongo
+from pymongo import MongoClient
+from pymongo import ASCENDING, DESCENDING
+
+client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
+
+try:
+    client.cloudly.authenticate(settings.MONGO_USER, settings.MONGO_PASSWORD)
+except: pass
+
+mongo = client.cloudly
+
 
 if __name__ == "__main__":
     print "hello world from alertor"
