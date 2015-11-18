09bc17955aee2834c4d584bbc2b22de38bbe974e
Jan Paricka <jparicka@gmail.com>
2015-11-18 20:56:48 +0100

    added on custom motto on the web

--- dashboard/views.py
+++ dashboard/views.py
@@ -55,6 +55,7 @@ def home(request):
             "Real-time and very playful monitoring.",
             "Does the server monitoring for you like no other.",
             "Cheerful Servers Monitoring.",
+            "Old School Servers Monitoring.",
         ]
         return render_to_response('web.html', {'request':request,'CLOUDLY_MOTTOS':CLOUDLY_MOTTOS,}, context_instance=RequestContext(request))
 
