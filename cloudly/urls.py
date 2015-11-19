20b2abe373c721c7d965b8f5a709001beb2950e9
Jan Paricka <jparicka@gmail.com>
2015-11-19 21:13:10 +0100

    starting on server logs

--- cloudly/urls.py
+++ cloudly/urls.py
@@ -39,6 +39,9 @@ urlpatterns = patterns('',
     # servers incidents
     url(r'^incidents/$', 'incidents.views.incidents', name='incidents'),
 
+    # servers logs
+    # XXX
+
     # ajax
     url(r'^ajax/session/update/$', 'vms.views.update_session', name='update_session'),
     url(r'^ajax/cloud/vms/$', 'vms.views.ajax_virtual_machines', name='ajax_virtual_machines'),
