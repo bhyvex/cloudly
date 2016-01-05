0559731d07131358e2f2d9c7324a42e604303ead
Jan Paricka <jparicka@gmail.com>
2016-01-05 21:51:27 +0100

    temporarily disabling stargazers on the credits view

--- dashboard/views.py
+++ dashboard/views.py
@@ -159,8 +159,8 @@ def credits(request):
 
     STARGAZERS_URL = "https://api.github.comimport urllib2/repos/ProjectCloudly/Cloudly/stargazers"
 
-    stargazers = response = urllib2.urlopen(STARGAZERS_URL)
-    stargazers = stargazers.read()
+    #stargazers = response = urllib2.urlopen(STARGAZERS_URL)
+    #stargazers = stargazers.read()
 
     print request.user
     return render_to_response('credits.html', {'request':request,'stargazers':stargazers,}, context_instance=RequestContext(request))
