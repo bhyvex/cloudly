ccc6c5bbcdbd48e8f6d7a43fd64dd6a96f3ba913
Jan Paricka <jparicka@gmail.com>
2016-07-05 20:39:08 +0200

    added on a motto

--- dashboard/views.py
+++ dashboard/views.py
@@ -47,6 +47,7 @@ mongo = client.cloudly
 CLOUDLY_MOTTOS = [
     "The Power of Now!",
     "Details Matters!",
+    "Where is my monkey!",
     "Bulletproof monitoring out of the box.",
     "Save time and effort with our simple, reliable server monitoring.",
     "Get your servers into shape! 100% free for Open Source.",
