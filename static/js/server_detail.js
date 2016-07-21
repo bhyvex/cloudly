48effaf9b7f2b4425e0c3b215f6d47a9ea551331
Jan Paricka <jparicka@gmail.com>
2016-07-21 12:46:41 +0200

    and one more revert

--- static/js/server_detail.js
+++ static/js/server_detail.js
@@ -159,6 +159,7 @@ $(document).ready(function() {
     updateServerInfo();
     $('#serviceDiscovery').deactivePanel();
     $('#serverActivity').deactivePanel();
+    $('#activeNetworkSessions').deactivePanel();
     /*
     $('#networkingGraphs').deactivePanel();
     $('#diskGraphs').deactivePanel();
