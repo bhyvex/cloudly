14cf2d4cee1ac76ebe05581a38fee22b29fe5bb3
Jan Paricka <jparicka@gmail.com>
2016-11-30 14:22:21 +0100

    sc

--- agent.py
+++ agent.py
@@ -411,8 +411,8 @@ def _get_memory_usage():
         for line in ps.split('\n'):
             if('PhysMem' in line):
 
-                memory_used = int(line.split(' ')[1].replace('M',''))*1024*1000
-                memory_free = int(line.split(' ')[-2].replace('M',''))*1024*1000
+                memory_used = int(line.split(' ')[1].replace('M',''))*1024*1024
+                memory_free = int(line.split(' ')[-2].replace('M',''))*1024*1024
                 memory_total = memory_used + memory_free
 
 
