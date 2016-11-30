4326da537adf440e5083d296f41455d7156b6c20
Jan Paricka <jparicka@gmail.com>
2016-11-30 13:33:50 +0100

    added on mem readings on the agent

--- agent.py
+++ agent.py
@@ -406,34 +406,14 @@ def _get_memory_usage():
 
     if platform.system() == 'Darwin':
 
-        ps = subprocess.Popen(['ps', '-caxm', '-orss,comm'], stdout=subprocess.PIPE).communicate()[0]
-        vm = subprocess.Popen(['vm_stat'], stdout=subprocess.PIPE).communicate()[0]
+        ps = subprocess.Popen(['top','-l 1', '-s 0'], stdout=subprocess.PIPE).communicate()[0]
 
-        processLines = ps.split('\n')
-        sep = re.compile('[\s]+')
-        rssTotal = 0 # kB
-        for row in range(1,len(processLines)):
-            rowText = processLines[row].strip()
-            rowElements = sep.split(rowText)
-            try:
-                rss = float(rowElements[0]) * 1024
-            except:
-                rss = 0
-            rssTotal += rss
+        for line in ps.split('\n'):
+            if('PhysMem' in line):
 
-        vmLines = vm.split('\n')
-        sep = re.compile(':[\s]+')
-        vmStats = {}
-        for row in range(1,len(vmLines)-2):
-            rowText = vmLines[row].strip()
-            rowElements = sep.split(rowText)
-            vmStats[(rowElements[0])] = int(rowElements[1].strip('\.')) * 4096
-
-        print 'Wired Memory:\t\t%d MB' % ( vmStats["Pages wired down"]/1024 )
-        print 'Active Memory:\t\t%d MB' % ( vmStats["Pages active"]/1024 )
-        print 'Inactive Memory:\t%d MB' % ( vmStats["Pages inactive"]/1024 )
-        print 'Free Memory:\t\t%d MB' % ( vmStats["Pages free"]/1024 )
-        print 'Real Mem Total (ps):\t%.3f MB' % ( rssTotal/1024 )
+                memory_used = int(line.split(' ')[1].replace('M',''))*1024*1024/1000
+                memory_free = int(line.split(' ')[-2].replace('M',''))*1024*1024/1000
+                memory_total = memory_used + memory_free
 
 
     else:
@@ -452,6 +432,7 @@ def _get_memory_usage():
             memory_total = -1
             memory_free = -1
             memory_used = -1
+
 
     memory_usage = {
         'memory_total': memory_total,
