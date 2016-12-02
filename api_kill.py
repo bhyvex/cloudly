bb86eb072889aeda350cc48faa8d51553fc9d3cb
Jan Paricka <jparicka@gmail.com>
2016-12-02 17:32:34 +0100

    finished off kill api

--- api_kill.py
+++ api_kill.py
@@ -5,4 +5,7 @@ ps = subprocess.Popen(('ps', 'aux'), stdout=subprocess.PIPE, close_fds=True).com
 
 for line in ps.split('\n'):
     if "api.py" in line:
-        print line
+
+        regexp = re.findall(r'(\b[^\s]+\b)', line)
+        pid = regexp[1]
+        os.system('kill -9 ' + pid)
