f8d4be2fda64444273a746596ede4e73958ab78f
Jan Paricka <jparicka@gmail.com>
2016-12-02 17:34:28 +0100

    and one more finished off kill api

--- api_kill.py
+++ api_kill.py
@@ -1,11 +1,17 @@
 import re, os
 import subprocess
 
-ps = subprocess.Popen(('ps', 'aux'), stdout=subprocess.PIPE, close_fds=True).communicate()[0]
 
-for line in ps.split('\n'):
-    if "api.py" in line:
+while True:
 
-        regexp = re.findall(r'(\b[^\s]+\b)', line)
-        pid = regexp[1]
-        os.system('kill -9 ' + pid)
+    ps = subprocess.Popen(('ps', 'aux'), stdout=subprocess.PIPE, close_fds=True).communicate()[0]
+
+    for line in ps.split('\n'):
+        if "api.py" in line:
+
+            regexp = re.findall(r'(\b[^\s]+\b)', line)
+            pid = regexp[1]
+            os.system('kill -9 ' + pid)
+            print 'api killed...'
+
+    time.sleep(15)
